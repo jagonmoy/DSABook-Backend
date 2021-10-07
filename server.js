3b1e3f1b035b4739629623b92ef29a1c96390356
Jagonmoy <jagonmoydey1997@gmail.com>
2021-10-07 18:08:24 +0600

    Adding Database Connection

--- server.js
+++ server.js
@@ -1,5 +1,24 @@
+const mongoose = require('mongoose');
 const app = require('./app');
-const port = 3005 ;
+const dotenv = require('dotenv');
+
+dotenv.config({path : './config.env'})
+
+
+const DatabaseConnection = async () => {
+    await mongoose.connect(process.env.DATABASE,{
+        useNewUrlParser : true ,
+        useCreateIndex: true ,
+        useFindAndModify: false
+    });
+    console.log("Database is Connected")
+}
+DatabaseConnection();
+
+
+
+const port = 3010 || process.env.PORT ;
+
 app.listen(port, () => {
     console.log(`${port} is running`);
 });
