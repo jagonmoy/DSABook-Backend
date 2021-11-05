01f127e1b1a20b467a629884bc35a3e7eeecab24
Jagonmoy <jagonmoydey1997@gmail.com>
2021-11-05 19:32:33 +0600

    add unit test for controller

--- controller/blogController.js
+++ controller/blogController.js
@@ -15,7 +15,7 @@ exports.getAllBlogs = async (req, res) => {
       else return contentNegotiation.sendBlogResponse(200,blogs,req,res)
   } 
   catch (error) { 
-    contentNegotiation.sendBlogResponse(404,error.message,req,res)    
+    return contentNegotiation.sendBlogResponse(404,error.message,req,res)    
   }
 };
 
@@ -24,11 +24,10 @@ exports.getBlog = async (req, res) => {
   try {
     const blog = await blogService.getBlog(req);
     if(typeof blog === "string") return contentNegotiation.sendBlogResponse(404,blog,req,res)
-    else return contentNegotiation.sendBlogResponse(200,blog,req,res)
+    return contentNegotiation.sendBlogResponse(200,blog,req,res)
   }
   catch (error) {
-    
-    contentNegotiation.sendBlogResponse(404,error.message,req,res) 
+    return contentNegotiation.sendBlogResponse(404,error.message,req,res) 
   }
 };
 
@@ -36,9 +35,9 @@ exports.getBlog = async (req, res) => {
 exports.createBlog = async (req, res) => {
   try {
     const newBlog = await blogService.createBlog(req);
-    contentNegotiation.sendBlogResponse(201,newBlog,req,res)
+    return contentNegotiation.sendBlogResponse(201,newBlog,req,res)
   } catch (error) {
-    contentNegotiation.sendBlogResponse(404,"Blog Creation Unsuccessful",req,res) 
+    return contentNegotiation.sendBlogResponse(404,"Blog Creation Unsuccessful",req,res) 
   }
 };
 
@@ -48,9 +47,9 @@ exports.updateBlog = async (req, res) => {
     if (typeof blog === "string") return contentNegotiation.sendBlogResponse(404,blog,req,res)
     if (blog.username !== req.body.username) return contentNegotiation.sendBlogResponse(403,"Not Have permission to Update",req,res)
     blog = await blogService.updateBlog(req);
-    contentNegotiation.sendBlogResponse(403,blog,req,res)
-  } catch (err) {
-    contentNegotiation.sendBlogResponse(404,error.message,req,res)
+    return contentNegotiation.sendBlogResponse(200,blog,req,res)
+  } catch (error) {
+        return contentNegotiation.sendBlogResponse(404,error.message,req,res)
   }
 };
 
@@ -60,9 +59,9 @@ exports.deleteBlog = async (req, res) => {
     if (typeof blog === "string") return contentNegotiation.sendBlogResponse(404,blog,req,res)
     if (blog.username !== req.body.username) return contentNegotiation.sendBlogResponse(403, "Not Have permission to delete",req,res)
     await blogService.deleteBlog(req);
-    contentNegotiation.sendBlogResponse(403,"Blog Deleted",req,res)
-  } catch (err) {
-    contentNegotiation.sendBlogResponse(404,error.message,req,res)
+    return contentNegotiation.sendBlogResponse(200,"Blog Deleted",req,res)
+  } catch (error) {
+    return contentNegotiation.sendBlogResponse(404,error.message,req,res)
   }
 
 };
--- tests/controller/blogController.test.js
+++ tests/controller/blogController.test.js
@@ -15,64 +15,184 @@ const blog = {
     blogDescription: "some Blog",
 };
 
-jest.mock("../../node_modules/express-negotiate/index",() => {
-    return blog
-})
 const options = {
   body: {
     username: "someone",
     blogHeadline: "some Headline",
     blogDescription: "some Blog",
   },
-  headers: {
-      accept : "application/json" ,
-  },
-  negotiate : function(){return dorkari}
 }
 
-const blogs = [
-  {
-    username: "someone",
-    blogHeadline: "some Headline",
-    blogDescription: "some Blog",
-  },
-];
-const createdBlog = {
-  username: "someone",
-  blogHeadline: "some Headline",
-  blogDescription: "some Blog",
-};
-const updatedBlog = {
-  username: "someone 2",
-  blogHeadline: "some Headline 2 ",
-  blogDescription: "some Blog 2",
-};
 
+ test(
+   "Testing Get Blog Method of Blog Service Class When There is No Error",
+   sinontest(async function () {
+     const req = mockRequest(options); 
+     const getBlogFromServiceStub = this.stub(blogService,"getBlog").returns(blog);
+     const getBlogFromContentNegotiationStub = this.stub(contentNegotiation,"sendBlogResponse").returns(blog); 
+     const result = await blogController.getBlog(req);  
+     expect(result).toEqual(blog)
+     sinon.assert.calledOnce(getBlogFromServiceStub);
+     sinon.assert.calledOnce(getBlogFromContentNegotiationStub);
+     sinon.assert.calledWithExactly(getBlogFromServiceStub,req);
+     sinon.assert.calledWithExactly(getBlogFromContentNegotiationStub,200,blog,req,undefined); 
+   })
+ );
+ test(
+   "Testing Get Blog Method of Blog Service Class When There is Error",
+   sinontest(async function () {
+     const req = mockRequest(options); 
+     const getBlogFromServiceStub = this.stub(blogService,"getBlog").returns(Promise.reject(new Error("Random")));
+     const getBlogFromContentNegotiationStub = this.stub(contentNegotiation,"sendBlogResponse").returns("Error") 
+     const result = await blogController.getBlog(req); 
+     expect(result).toEqual("Error") 
+     sinon.assert.calledOnce(getBlogFromServiceStub);
+     sinon.assert.calledOnce(getBlogFromContentNegotiationStub);
+     sinon.assert.calledWithExactly(getBlogFromServiceStub,req);
+     sinon.assert.calledWithExactly(getBlogFromContentNegotiationStub,404,"Random",req,undefined);
+   })
+ ); 
+ test(
+   "Testing Get All Blogs Method of Blog Service Class When There is No Error",
+   sinontest(async function () {
+     const req = mockRequest(options);
+     
+     const getAllBlogsFromServiceStub = this.stub(blogService,"getAllBlogs").returns(blog);
+     const getAllBlogsFromContentNegotiationStub = this.stub(contentNegotiation,"sendBlogResponse").returns(blog) 
+     const result = await blogController.getAllBlogs(req);
+     expect(result).toEqual(blog)
+     sinon.assert.calledOnce(getAllBlogsFromServiceStub);
+     sinon.assert.calledOnce(getAllBlogsFromContentNegotiationStub);
+     sinon.assert.calledWithExactly(getAllBlogsFromServiceStub,req);
+     sinon.assert.calledWithExactly(getAllBlogsFromContentNegotiationStub,200,blog,req,undefined); 
+   })
+ );
 test(
-  "Testing Get Blog Method of Blog Service Class",
+  "Testing Get All Blogs Method of Blog Service Class When There is Error",
   sinontest(async function () {
     const req = mockRequest(options);
-    const getBlogStub = this.stub(blogService,"getBlog");
-    getBlogStub.returns(blog);
-    const somethingStub = this.stub(contentNegotiation,"sendBlogResponse").returns(blog)
+    const getAllBlogsFromServiceStub = this.stub(blogService,"getAllBlogs").returns(Promise.reject(new Error("Random")));
+    const getAllBlogsFromContentNegotiationStub = this.stub(contentNegotiation,"sendBlogResponse").returns("Error")
 
-    const res = await blogController.getBlog(req);
-    expect(res).toEqual(blog)
-    //expect(blogService.getBlog()).toEqual(blog);
-    //expect(req.negotiate).toEqual('success');
+    const result = await blogController.getAllBlogs(req);
+    
+    expect(result).toEqual("Error")
+    sinon.assert.calledOnce(getAllBlogsFromServiceStub);
+    sinon.assert.calledOnce(getAllBlogsFromContentNegotiationStub);
+    sinon.assert.calledWithExactly(getAllBlogsFromServiceStub,req);
+    sinon.assert.calledWithExactly(getAllBlogsFromContentNegotiationStub,404,"Random",req,undefined);
   })
 );
 test(
-  "Testing Get Blog Method of Blog Service Class 2",
+  "Testing create blog Method of Blog Service Class When There is No Error",
   sinontest(async function () {
     const req = mockRequest(options);
-    const getBlogStub = this.stub(blogService,"getBlog");
-    getBlogStub.returns("hello");
-    const somethingStub = this.stub(contentNegotiation,"sendBlogResponse").returns(new Error("hello"))
-
-    const res = await blogController.getBlog(req);
-    expect(res).toEqual(Error("hello"))
-    //expect(blogService.getBlog()).toEqual(blog);
-    //expect(req.negotiate).toEqual('success');
+    
+    const createBlogFromServiceStub = this.stub(blogService,"createBlog").returns(blog);
+    const createBlogFromContentNegotiationStub = this.stub(contentNegotiation,"sendBlogResponse").returns(blog) 
+    const result = await blogController.createBlog(req);
+    expect(result).toEqual(blog)
+    sinon.assert.calledOnce(createBlogFromServiceStub);
+    sinon.assert.calledOnce(createBlogFromContentNegotiationStub);
+    sinon.assert.calledWithExactly(createBlogFromServiceStub,req);
+    sinon.assert.calledWithExactly(createBlogFromContentNegotiationStub,201,blog,req,undefined); 
   })
 );
+test(
+ "Testing create blog Method of Blog Service Class When There is Error",
+ sinontest(async function () {
+   const req = mockRequest(options);
+   const createBlogFromServiceStub = this.stub(blogService,"createBlog").returns(Promise.reject(new Error("Random")));
+   const createBlogFromContentNegotiationStub = this.stub(contentNegotiation,"sendBlogResponse").returns("Error")
+
+   const result = await blogController.createBlog(req);
+   
+   expect(result).toEqual("Error")
+   sinon.assert.calledOnce(createBlogFromServiceStub);
+   sinon.assert.calledOnce(createBlogFromContentNegotiationStub);
+   sinon.assert.calledWithExactly(createBlogFromServiceStub,req);
+   sinon.assert.calledWithExactly(createBlogFromContentNegotiationStub,404,"Blog Creation Unsuccessful",req,undefined);
+ })
+);
+test(
+  "Testing update blog Method of Blog Service Class When There is No Error",
+  sinontest(async function () {
+    const req = mockRequest(options);
+    
+    const getBlogFromServiceStub = this.stub(blogService,"getBlog").returns(blog);
+    const updateBlogFromServiceStub = this.stub(blogService,"updateBlog").returns(blog);
+    const updateBlogFromContentNegotiationStub = this.stub(contentNegotiation,"sendBlogResponse").returns(blog) 
+
+    const result = await blogController.updateBlog(req);
+
+    expect(result).toEqual(blog)
+    sinon.assert.calledOnce(getBlogFromServiceStub);
+    sinon.assert.calledOnce(updateBlogFromServiceStub);
+    sinon.assert.calledOnce(updateBlogFromContentNegotiationStub);
+    sinon.assert.calledWithExactly(getBlogFromServiceStub,req);
+    sinon.assert.calledWithExactly(updateBlogFromServiceStub,req);
+    sinon.assert.calledWithExactly(updateBlogFromContentNegotiationStub,200,blog,req,undefined); 
+
+  })
+);
+test(
+ "Testing update blog Method of Blog Service Class When There is Error",
+ sinontest(async function () {
+   const req = mockRequest(options);
+   
+   const getBlogFromServiceStub = this.stub(blogService,"getBlog").returns(Promise.reject(new Error("Random")));
+   const updateBlogFromServiceStub = this.stub(blogService,"updateBlog").returns(blog);
+   const updateBlogFromContentNegotiationStub = this.stub(contentNegotiation,"sendBlogResponse").returns("Error")
+
+
+   const result = await blogController.updateBlog(req);
+   
+   expect(result).toEqual("Error")
+   sinon.assert.calledOnce(getBlogFromServiceStub);
+   sinon.assert.calledWithExactly(getBlogFromServiceStub,req);
+   sinon.assert.calledWithExactly(updateBlogFromContentNegotiationStub,404,"Random",req,undefined);
+ })
+);
+
+test(
+  "Testing delete blog Method of Blog Service Class When There is No Error",
+  sinontest(async function () {
+    const req = mockRequest(options);
+    
+    const getBlogFromServiceStub = this.stub(blogService,"getBlog").returns(blog);
+    const deleteBlogFromServiceStub = this.stub(blogService,"deleteBlog").returns(blog);
+    const deleteBlogFromContentNegotiationStub = this.stub(contentNegotiation,"sendBlogResponse").returns(blog) 
+
+    const result = await blogController.deleteBlog(req);
+
+    expect(result).toEqual(blog)
+    sinon.assert.calledOnce(getBlogFromServiceStub);
+    sinon.assert.calledOnce(deleteBlogFromServiceStub);
+    sinon.assert.calledOnce(deleteBlogFromContentNegotiationStub);
+    sinon.assert.calledWithExactly(getBlogFromServiceStub,req);
+    sinon.assert.calledWithExactly(deleteBlogFromServiceStub,req);
+    sinon.assert.calledWithExactly(deleteBlogFromContentNegotiationStub,200,"Blog Deleted",req,undefined); 
+
+  })
+);
+test(
+ "Testing delete blog Method of Blog Service Class When There is Error",
+ sinontest(async function () {
+   const req = mockRequest(options);
+   
+   const getBlogFromServiceStub = this.stub(blogService,"getBlog").returns(Promise.reject(new Error("Random")));
+   const deleteBlogFromServiceStub = this.stub(blogService,"deleteBlog").returns(blog);
+   const deleteBlogFromContentNegotiationStub = this.stub(contentNegotiation,"sendBlogResponse").returns("Error")
+
+
+   const result = await blogController.deleteBlog(req);
+   
+   expect(result).toEqual("Error")
+   sinon.assert.calledOnce(getBlogFromServiceStub);
+   sinon.assert.calledWithExactly(getBlogFromServiceStub,req);
+   sinon.assert.calledWithExactly(deleteBlogFromContentNegotiationStub,404,"Random",req,undefined);
+ })
+);
+
+
+
