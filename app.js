545b03a3fe7a4bcadfdc422e3c9c3ebe597c28f0
Jagonmoy <jagonmoydey1997@gmail.com>
2021-12-06 17:09:55 +0600

    fix cookie issue

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(
 app.use(
     cors({
       credentials: true,
-      origin: 'https://dsa-book-frontend.herokuapp.com/'
+      origin: 'http://localhost:3000/'
     })
   );
 app.use(morgan('dev'));
