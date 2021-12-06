6e4fced28a057bb0c9c3b569888889523628fed8
Jagonmoy <jagonmoydey1997@gmail.com>
2021-12-06 17:33:29 +0600

    fixed cookie issue

--- app.js
+++ app.js
@@ -8,12 +8,23 @@ const blogRouter = require('./routes/blogRoutes');
 const userRouter = require('./routes/userRoutes');
 const authRouter = require('./routes/authRoutes');
 const morgan = require('morgan');
-
+// const path = require('path');
 
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser())
+// app.use(
+//     session({
+//       secret: process.env.SESSION_SECRET || 'anysecret',
+//       resave: true,
+//       saveUninitialized: false,
+//       cookie: {
+//         sameSite: process.env.NODE_ENV === "production" ? 'none' : 'lax', // must be 'none' to enable cross-site delivery
+//         secure: process.env.NODE_ENV === "production", // must be true if sameSite='none'
+//       }
+//     })
+// );
 
 app.use(
     cors({
