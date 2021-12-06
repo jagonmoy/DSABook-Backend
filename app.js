eeaff0491b1540ac219b17f23f2f12429d0a1e83
Jagonmoy <jagonmoydey1997@gmail.com>
2021-12-06 17:57:34 +0600

    try fixing cookie issue

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(cookieParser())
 app.use(
     cors({
       credentials: true,
-      origin: 'https://dsa-book-frontend.herokuapp.com'
+      origin: 'true'
     })
   );
 app.use(morgan('dev'));
