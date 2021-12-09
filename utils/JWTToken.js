2213bce599d3a0746f6011d19843d898fa5718c4
Jagonmoy <jagonmoydey1997@gmail.com>
2021-12-09 17:10:52 +0600

    add environment based condition for cookie

--- utils/JWTToken.js
+++ utils/JWTToken.js
@@ -16,7 +16,8 @@ exports.sendJWTToken = (username) => {
         ),
         httponly: true,
       };
-    } else {
+    } 
+    else if ( process.env.NODE_ENV === 'production') {
       cookieOptions = {
         expires: new Date(
           Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000
@@ -36,7 +37,8 @@ exports.clearToken = () => {
       expires: new Date(Date.now() + 1* 1000),
       httponly: true,
     };
-  } else {
+  } 
+  else if ( process.env.NODE_ENV === 'production') {
     cookieOptions = {
       expires: new Date(Date.now() + 1* 1000),
       secure: true,
