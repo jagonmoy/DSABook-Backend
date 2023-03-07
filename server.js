f08f9773e4f693b05cd4a2fd675e495bef9dc989
Jagonmoy <jagonmoydey1997@gmail.com>
2023-03-07 12:53:08 +0600

    fix(database connecton): added some parameters in connect function of mongoose

--- server.js
+++ server.js
@@ -3,9 +3,15 @@ const dotenv = require('dotenv');
 dotenv.config({path : './config.env'})
 const app = require('./app');
 
-const DatabaseConnection = async () => {
-    await mongoose.connect(process.env.DATABASE);
-    console.log("Database is Connected")
+const DatabaseConnection = () => {
+    mongoose.connect(process.env.DATABASE,{
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true
+    }).then(con => {
+        console.log('Database is Connected');
+    });
 }
 DatabaseConnection();
 
